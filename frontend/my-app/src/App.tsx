d0741f90999e4d45b37e1eb5e58dc2bae7f41ea6
Bimbisara P <bimbisara13@gmail.com>
2026-01-06 21:19:34 +0530

    New: Initialize frontend with React, TypeScript, and Vite setup.

---
+++ frontend/my-app/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-50">
+        <p className="text-5xl font-normal">Auth Portal</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
